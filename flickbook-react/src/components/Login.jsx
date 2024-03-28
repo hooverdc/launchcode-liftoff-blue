6ed6ad067fb04e782f802faeb7ae098cd37cd20a
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2024-03-27 23:12:12 -0500

    Update Login.jsx

--- flickbook-react/src/components/Login.jsx
+++ flickbook-react/src/components/Login.jsx
@@ -19,7 +19,59 @@ export default function Login() {
         <div class="register-link">
             <p>Don't have an account? <a href="#">Register</a></p>
       </div>
-      </form>
+      </form> 
+         </form>
+      <form id='register' class='input-group-register'>
+       <input type='text' class='input-field' placeholder='First Name' required>
+       <input type='text' class='input-field' placeholder='Last Name' required>
+       <input type='email' class='input-field' placeholder='Email ID' required>
+       <input type='password' class='input-field' placeholder='Enter Password' required>
+       <input type='password' class='input-field' placeholder='Confirm Password' required>
+      <input type="Username" class='input-field' placeholder='Username' required>
+           <input type='checkbox' class='check-box'></input>
+           <form> </form>
+           <span> I agree to the terms and conditions </span>
+       <button type='submit' class='submit-btn' >Register</button>
+      <div></div>
+           <div id='login-form' class='login-page'>
+            <div class="form-box">
+              <div class='button-box'>
+              <div id='btn'></div>
+              <button
+              type='button'onclick='register()'class='btn'>Register</button>
+              </div>
+          </div>
+      <script>
+           var x=document.getElementById('login');
+           var y=document.getElementById('register');
+           var z=document.getElementById('btn');
+      function register ()
+              {
+                     x.style.left='-400px'}
+                     y.style.left='50px'
+                     z.style.left='110px'
+              }
+
+              function login()
+               {
+                 x.style.left='50px'}
+                 y.style.left='450px';
+                 z.style.left='0px';
+               }
+      </script>
+      <script>
+                var modal= document.getElementById('login-form');
+                window.onclick = function(event)
+                {
+                   (event.target == modal)
+               {
+                    then modal.style.display = 'none';
+                }
+
+       };
+</script>
+   );
+};
       </div>
    );
 }
