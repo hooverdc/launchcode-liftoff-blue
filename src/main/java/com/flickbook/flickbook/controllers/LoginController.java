f02c052c15f631c3cdf13e497130992b85e0f083
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2024-03-27 23:16:17 -0500

    Update LoginController.java

--- src/main/java/com/flickbook/flickbook/controllers/LoginController.java
+++ src/main/java/com/flickbook/flickbook/controllers/LoginController.java
@@ -1,4 +1,27 @@
 package com.flickbook.flickbook.controllers;
 
+
+import com.flickbook.flickbook.models.Movie;
+import com.flickbook.flickbook.models.Review;
+import com.flickbook.flickbook.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class LoginController {
 }
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @Autowired
+    private LoginController LoginController;
+
+    @Autowired
+    private Regitration Registration;
+
+    @Autowired Username Username;
