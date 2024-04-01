d26aa6c6850426f06328dbe4043af1636be3696d
Tippacanoe88 <137940796+Tippacanoe88@users.noreply.github.com>
2024-04-01 18:25:34 -0500

    Added functionality for adding movies in the UserController.java and UserProfile.jsx. added one to many relationship on User.java

--- flickbook-react/src/App.jsx
+++ flickbook-react/src/App.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import { useState } from 'react'
 import './App.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar"
 import Login from "./components/Login"
+import { Typography } from '@material-ui/core';
 
-function App() {
+function App() => {
 
   return (
     <>
 			<div>
+					<Typography variant="h1">Flickbook</Typography>
 					<Navbar />
 					<Login />
 
