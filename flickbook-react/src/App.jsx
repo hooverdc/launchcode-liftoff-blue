461b18066b5d6976c72a4f6b3bb43c0a40d5783b
Tippacanoe88 <137940796+Tippacanoe88@users.noreply.github.com>
2024-04-01 20:09:08 -0500

    App.jsx changes for improving the UI on the home page

--- flickbook-react/src/App.jsx
+++ flickbook-react/src/App.jsx
@@ -4,18 +4,47 @@ import './App.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar"
 import Login from "./components/Login"
-import { Typography } from '@material-ui/core';
-
-function App() => {
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+import { ThumbUp, ThumbDown, LibraryAdd, Home, AddComment } from '@mui/icons-material';
+function App() {
 
   return (
     <>
-			<div>
-					<Typography variant="h1">Flickbook</Typography>
-					<Navbar />
-					<Login />
-
-			</div>
+	<CssBaseline />
+			<AppBar position="relative">
+				<Toolbar>
+					<Home />
+					<Typography variant="h6">
+						Home
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+				<div>
+					<Container maxWidth="sm">
+						<Typography variant="h2" align="center" color="textPrimary" gutterbottom>
+							Home 
+						</Typography>
+						<Typography variant="h5" align="center" color="textSecondary" paragraph>
+							All the movies we think you might like!
+						</Typography>
+						<div>
+							<Grid container spacing={2} justify="center">
+								<Grid item>
+									<Button variant="contained">
+										Movies to watch
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button variant="outlined" color="primary">
+										Secondary action
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
+				</div>
+			</main>
     </>
   )
 }
