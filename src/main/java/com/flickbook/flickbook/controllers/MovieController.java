8498e6b3d1cd3817f6d14b001196df0e926a30b1
Jimmy <hungtran2003ti@gmail.com>
2024-04-11 18:50:31 -0500

    Add api prefix to pathname api cal

--- src/main/java/com/flickbook/flickbook/controllers/MovieController.java
+++ src/main/java/com/flickbook/flickbook/controllers/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movie")
+@RequestMapping("api/movie")
 public class MovieController {
     @Autowired
     private MovieService movieService;
--- src/main/java/com/flickbook/flickbook/controllers/UserController.java
+++ src/main/java/com/flickbook/flickbook/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("api/user")
 public class UserController {
   @Autowired
   private UserService userService;
