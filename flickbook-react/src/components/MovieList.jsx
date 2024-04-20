dc385515c867dcad78c6dfd04c01a65fce5ffdfc
timconceivable <headskape@yahoo.com>
2024-04-20 16:26:08 -0500

    implemented TMDB api in search

--- flickbook-react/src/components/MovieList.jsx
+++ flickbook-react/src/components/MovieList.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 export default function MovieList (props) {
+let poster_base = "https://image.tmdb.org/t/p/w200/"; //TMDB base img url
 	return (
 		<>
-			{props.movies.map((movie, index) => (
+			{ 
+			(props.movies.length==0) ? 
+				<p className="notfound">Not Found</p> : 
+				props.movies.map((movie, index) => (
 				<div className="img-box">
-					{movie.Poster !== "N/A" ? 	 
-						<img src={movie.Poster} alt={movie.Title}></img> : 
+					{movie.poster_path !== null ? //OMDB returns "N/A" instead of null
+						<img src={poster_base+movie.poster_path} alt={movie.title}></img> : 
 						<div className="noimg">
 							<p>NO IMAGE AVAILABLE</p>
 						</div>}
-					<p>{movie.Title} ({movie.Year}{movie.Type !== "movie" ? ` ${movie.Type})` : `)`}</p>
+					<p>{movie.title} ({movie.release_date.slice(0,4)}) 
+{/* 					({movie.Year}{movie.Type !== "movie" ? ` ${movie.Type})` : `)`} */}
+					</p>
 				</div>
 			))}
 		</>
--- flickbook-react/src/components/Search.jsx
+++ flickbook-react/src/components/Search.jsx
@@ -5,14 +5,14 @@ export default function Search(props) {
 	const [searchTerm, setSearchTerm] = useState('');
 	
 	const getSearchResults = async (searchTerm) => { 
-		const url = 'https://www.omdbapi.com/?s='+searchTerm+'&apikey=263d22d8';
-// 		const url = 'https://api.themoviedb.org/3/search/movie?query='
-// 			+searchTerm+'&api_key=c9f28a74e58f0319e5b5844496ca4d3a';
+// 		const url = 'https://www.omdbapi.com/?s='+searchTerm+'&apikey=263d22d8';
+		const url = 'https://api.themoviedb.org/3/search/movie?query='
+			+searchTerm+'&api_key=c9f28a74e58f0319e5b5844496ca4d3a';
 		const response = await fetch(url);
 		const responseJson = await response.json();
 	
-		if (responseJson.Search) {
-			props.setMovies(responseJson.Search);
+		if (responseJson.results) { //OMDB uses .Search instead of results
+			props.setMovies(responseJson.results);
 		}
 	}
 	
