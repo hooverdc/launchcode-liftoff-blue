a360d9429d2b623779903ffa3ff5595ff81ce05b
timconceivable <headskape@yahoo.com>
2024-04-22 23:27:34 -0500

    moved code from Home.jsx into LikedList.jsx

--- flickbook-react/src/components/LikedList.jsx
+++ flickbook-react/src/components/LikedList.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 export default function LikedList() {
-	
+
+	// 	useEffect(() => {
+  // 		const fetchLikedMovies = async () => {
+  // 			try {
+  // 				const response = await axios.get('http://localhost:8080/api/moviesLiked'); // Replace with your actual endpoint
+  // 				setLikedMovies(response.data);
+  // 			} catch (error) {
+  // 				console.error('Failed to fetch liked movies:', error);
+  // 			}
+  // 		};
+  // 
+  // 		fetchLikedMovies();
+  // 	}, []);
+      
 	return (
 		<>
 		<p>This is your Liked List page</p>
+  {/* 		{likedMovies.map(movie => ( */}
+  {/* 		<div key={movie.id}> */}
+  {/* 		<h2>{movie.title}</h2> */}
+  {/* 		</div> */}
+  {/* 		))} */}
 		</>
 	);
 }
